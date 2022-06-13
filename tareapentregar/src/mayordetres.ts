2e25bba68b734baa778c3fc9ce726e4c96d5990c
yamiladib <104107990+yamiladib@users.noreply.github.com>
2022-06-12 22:52:56 -0300

    mayor de 3

--- tareapentregar/src/mayordetres.ts
+++ tareapentregar/src/mayordetres.ts
@@ -1,27 +1,25 @@
-let rotulo = document.getElementById("rotulo");
-let rotulo1 = document.getElementById("rotulo1");
-let rotulo2 = document.getElementById("rotulo2");
-
-let btnEnv = document.getElementById("btnEnviar");
-let dato = document.getElementById("dato");
+//mayorde3
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
+let dato3 = document.getElementById("dato3");
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+let rotulo3 = document.getElementById("rotulo3");
 
-rotulo1.innerHTML = ("Ingrese numero 1");
-rotulo2.innerHTML = ("Ingrese numero 2");
-rotulo3.innerHTML = ("Ingrese numero 3");
+rotulo1.innerHTML = "ingrese primer numero:";
+rotulo2.innerHTML = "ingrese segundo numero:";
+rotulo3.innerHTML = "ingrese tercer numero:";
 
-btnEnviar.addEventListener("click", () => {
-  let numero1:number=Number(dato.value);
-  let numero2:number=Number(dato1.value);
-  let numero3:number=Number(dato2.value); 
-   
-  if (numero1 > numero2 && numero1>numero3){
-    console.log ("el numero1 numero es el mayor");  
-  } else if (numero2 > numero1 && numero2>numero3){ 
-   console.log ("el numero2 numero es el mayor");
-} else if  (numero3>numero1 && numero3>numero2) {
-  console.log ("el numero3 numero es el mayor")}
-  
-  }
+let btnEnv = document.getElementById("btnEnviar");
+
+btnEnv.addEventListener("click", () => {
+  let numeroUno: number = Number(dato1.value);
+  let numeroDos: number = Number(dato2.value);
+  let numeroTres: number = Number(dato3.value);
+  if (numeroUno > numeroDos && numeroUno > numeroTres) {
+    console.log("el" + numeroUno + "es el mayor");
+  } else if (numeroDos > numeroUno && numeroDos > numeroTres) {
+    console.log("el" + numeroDos + "es el mayor");
+  } else if (numeroTres > numeroUno && numeroTres > numeroDos)
+    console.log("el" + numeroTres + "es el mayor");
 });
